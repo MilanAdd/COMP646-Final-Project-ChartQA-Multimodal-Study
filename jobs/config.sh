60dc6098abf5def76e44349b4adf92219e1e1929
Milan A <milu4usa@gmail.com>
2026-04-16 16:44:30 -0500

    removed

--- jobs/config.sh
+++
@@ -1,5 +0,0 @@
-# NOTS cluster configuration — edit this file with your settings.
-NETID="ma200"
-REMOTE="${NETID}@nots.rice.edu"
-REMOTE_DIR="/storage/hpc/work/${NETID}/chartqa_project"
-WORK="/storage/hpc/work/${NETID}"
